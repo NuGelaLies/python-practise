bb1dbcefacef0cbb4d4052834badc6753f60b50a
NuGelaLies <NuGelaLies@126.com>
2018-06-30 10:57:45 +0800

    - practise for Day1

---
+++ Day1/turtle.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+#turtle.mainloop()
+    
+
+turtle.pensize(4)
+turtle.pencolor('green')
+turtle.forward(400)
+turtle.right(90)
+turtle.forward(400)
+turtle.right(90)
+turtle.forward(400)
+turtle.right(90)
+turtle.forward(400)
+turtle.mainloop()
---
+++ Day1/type.py
@@ -0,0 +1,35 @@
+a = 100
+b = 100000000000000000
+c = 12.35
+d = 1 + 5j
+e = 'A'
+f = 'hello world'
+g = True
+
+'''
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+
+'''
+
+a1 = str(a)
+b1 = str(b)
+c1 = str(c)
+d1 = bool(g)
+e1 = str(g)
+f1 = '123456'
+f2 = int(f1)
+
+
+
+print(type(a1))
+print(type(b1))
+print(type(c1))
+print(type(d1))
+print(type(e1))
+print(type(f2))
